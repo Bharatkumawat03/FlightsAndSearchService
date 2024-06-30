374547f736aaa1972e55cdc7f6deb0686b2a39c8
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-06-30 17:41:35 +0530

    Added city service

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,54 @@
+const { where } = require('sequelize');
 const {City} = require('../models/index');
 
 class CityRepository{
+    
     async createCity({name}){
-        try{
-            const city = await City.create({name});
+        try {
+            const city = await City.create({
+                name
+            });
             return city;
-        } catch(error){
+        } catch (error) {
+            console.log("Something get wrong in the repository layer");
             throw(error);
         }
     }
 
     async deleteCity(cityId){
-        try{
+        try {
             await City.destroy({
                 where: {
+                    id: cityId 
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("Something get wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data, {
+                where:{
                     id: cityId
                 }
             });
-        }catch(error){
+            return city;
+        } catch (error) {
+            console.log("Something get wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something get wrong in the repository layer");
             throw(error);
         }
     }
