08faf5e804863bf719cced91d14f122367ef16ce
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-07-03 09:45:39 +0530

    Added more routers to complete create api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -25,7 +25,7 @@ const create = async (req, res) => {
 
 const destroy = async (req, res) => {
     try {
-        const city = await cityService.deleteCity(req.params.id);
+        const response = await cityService.deleteCity(req.params.id);
         return res.status(200).json({
             data: response,
             success: true,
@@ -45,7 +45,7 @@ const destroy = async (req, res) => {
 
 const get = async (req, res) => {
     try {
-        const city = await cityService.getCity(req.params.id);
+        const response = await cityService.getCity(req.params.id);
         return res.status(200).json({
             data: response,
             success: true,
@@ -65,7 +65,7 @@ const get = async (req, res) => {
 
 const update = async (req, res) => {
     try {
-        const city = await cityService.updateCity(req.params.id, req.body);
+        const response = await cityService.updateCity(req.params.id, req.body);
         return res.status(201).json({
             data: response,
             success: true,
